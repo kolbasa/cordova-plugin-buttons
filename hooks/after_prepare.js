8a6c1b37b303f5b206bb4651454c758c686238a8
Michael Jedich <michael.jedich@pm.me>
2022-02-11 17:24:58 +0100

    Prepare hook added

---
+++ hooks/after_prepare.js
@@ -0,0 +1,45 @@
+module.exports = function (context) {
+
+    let fs = require('fs');
+    let path = require('path');
+    let UTF8 = 'utf8';
+
+    if (!context.opts.platforms.includes('android')) {
+        return;
+    }
+
+    const platformRoot = path.join(context.opts.projectRoot, 'platforms/android/app/src/main/java');
+    let configXmlPath = path.join(context.opts.projectRoot, 'config.xml');
+
+    let configXml = fs.readFileSync(configXmlPath, UTF8);
+    let appId = configXml.split('<widget id=\"').pop().split('\"')[0];
+    let packageName = appId.replace(/\./g, '/');
+
+    const MainActivity = platformRoot + '/' + packageName + '/MainActivity.java';
+    data = fs.readFileSync(MainActivity, UTF8);
+
+    if (data.includes('greenrobot')) {
+        return;
+    }
+
+    data = data.replace(
+        /import org.apache.cordova.*;/g,
+        'import org.apache.cordova.*;\n' +
+        'import android.view.KeyEvent;\n' +
+        'import org.greenrobot.eventbus.EventBus;'
+    );
+
+    data = data.replace(
+        /@Override/g,
+        '@Override\n' +
+        '    public boolean dispatchKeyEvent(KeyEvent keyEvent) {\n' +
+        '        boolean result = super.dispatchKeyEvent(keyEvent);\n' +
+        '        EventBus.getDefault().post(keyEvent);\n' +
+        '        return result;\n' +
+        '    }\n\n' +
+        '    @Override'
+    );
+
+    fs.writeFileSync(MainActivity, data, UTF8);
+
+};
