4b527e79a55aaadc17a7bdf451d730ec8e9d2377
Michael Jedich <michael.jedich@pm.me>
2022-02-11 17:25:40 +0100

    Cleanup + simple Readme

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 Cordova Buttons Plugin
 =========================
+
+This plugin allows you to listen to the hardware buttons on Android devices.
+
+# Installation
+
+### Cordova
+
+    cordova plugin add cordova-plugin-buttons
+
+### Ionic + Cordova
+
+    ionic cordova plugin add cordova-plugin-buttons
+
+### Capacitor
+
+    npm install cordova-plugin-buttons
+
+# API
+
+### subscribe()
+
+```js
+Buttons
+    .subscribe(function(button) {
+        // your callback
+    })
+    .then(function () {
+        // started
+    })
+    .catch(function (err) {
+        // something broke
+    });
+```
+
+### unsubscribe()
+
+```js
+Buttons
+    .unsubscribe()
+    .then(function () {
+        // stopped
+    })
+    .catch(function (err) {
+        // something broke
+    });
+```
--- src/android/Buttons.java
+++ src/android/Buttons.java
@@ -1,9 +1,5 @@
 package de.kolbasa.buttons;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.view.KeyEvent;
 
 import org.apache.cordova.CallbackContext;
@@ -15,62 +11,39 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 public class Buttons extends CordovaPlugin {
 
     private CallbackContext cbc;
-    private BroadcastReceiver broadcastReceiver;
+    private HashMap<Integer, Long> lastDownTime;
 
-    private void registerPowerButtonListener() {
-        unregisterGeneralButtonListener();
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
-        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
-        this.broadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String[] action = intent.getAction().split("\\.");
-                onKey(null, action[action.length - 1]);
-            }
-        };
-        this.webView.getContext().registerReceiver(this.broadcastReceiver, intentFilter);
-    }
-
-    private void unregisterPowerButtonListener() {
-        if (this.broadcastReceiver != null) {
-            this.webView.getContext().unregisterReceiver(this.broadcastReceiver);
-            this.broadcastReceiver = null;
-        }
-    }
-
-    private void registerGeneralButtonListener() {
-        EventBus.getDefault().register(this);
-    }
-
-    private void unregisterGeneralButtonListener() {
-        EventBus.getDefault().unregister(this);
-    }
-
-    private long lastDownTime;
-
-    private void onKey(KeyEvent keyEvent, String action) {
+    @Subscribe
+    @SuppressWarnings("unused")
+    public void onEvent(final KeyEvent keyEvent) {
         JSONObject json = new JSONObject();
+
+        if (this.lastDownTime == null) {
+            this.lastDownTime = new HashMap<>();
+        }
 
         try {
             if (keyEvent != null) {
-                if (this.lastDownTime == keyEvent.getDownTime() &&
-                        keyEvent.getAction() == 0 && !keyEvent.isCanceled()) {
+                int keyCode = keyEvent.getKeyCode();
+                Long lastUsed = lastDownTime.get(keyCode);
+
+                if (lastUsed != null && lastUsed == keyEvent.getDownTime() &&
+                        keyEvent.getAction() == KeyEvent.ACTION_DOWN && !keyEvent.isCanceled()) {
                     return;
                 }
+
                 json.put("keyCode", keyEvent.getKeyCode());
                 json.put("eventTime", keyEvent.getEventTime());
                 json.put("downTime", keyEvent.getDownTime());
                 json.put("cancelled", keyEvent.isCanceled());
                 json.put("finished", keyEvent.getAction() == KeyEvent.ACTION_UP);
-                this.lastDownTime = keyEvent.getDownTime();
-            }
 
-            if (action != null) {
-                json.put("action", action);
+                this.lastDownTime.put(keyCode, keyEvent.getDownTime());
             }
         } catch (JSONException e) {
             //
@@ -81,17 +54,12 @@ public class Buttons extends CordovaPlugin {
         this.cbc.sendPluginResult(response);
     }
 
-    @Subscribe
-    @SuppressWarnings("unused")
-    public void onEvent(final KeyEvent event) {
-        onKey(event, null);
+    private void registerGeneralButtonListener() {
+        EventBus.getDefault().register(this);
     }
 
-    private void unregisterCallback() {
-        if (this.cbc != null) {
-            this.cbc.success("Unsubscribed");
-            this.cbc = null;
-        }
+    private void unregisterButtonListener() {
+        EventBus.getDefault().unregister(this);
     }
 
     private void registerCallback(CallbackContext callbackContext) {
@@ -102,9 +70,17 @@ public class Buttons extends CordovaPlugin {
         callbackContext.sendPluginResult(response);
     }
 
+    private void unregisterCallback() {
+        if (this.cbc != null) {
+            this.cbc.success("Callback closed");
+            this.cbc = null;
+        }
+        this.lastDownTime = null;
+    }
+
     private void subscribe(CallbackContext callbackContext) {
         try {
-            registerPowerButtonListener();
+            cleanup();
             registerGeneralButtonListener();
             registerCallback(callbackContext);
         } catch (Exception e) {
@@ -114,22 +90,21 @@ public class Buttons extends CordovaPlugin {
 
     private void unsubscribe(CallbackContext callbackContext) {
         try {
-            onDestroy();
+            cleanup();
             callbackContext.success();
         } catch (Exception e) {
             callbackContext.error(e.getMessage());
         }
     }
 
-    @Override
-    public void onDestroy() {
-        unregisterPowerButtonListener();
-        unregisterGeneralButtonListener();
-        super.onDestroy();
+    private void cleanup() {
+        unregisterButtonListener();
+        unregisterCallback();
     }
 
     @Override
-    public boolean execute(final String action, final JSONArray data, final CallbackContext callbackContext) {
+    public boolean execute(final String action, final JSONArray data,
+                           final CallbackContext callbackContext) {
         switch (action) {
             case "subscribe":
                 cordova.getThreadPool().execute(() -> subscribe(callbackContext));
